fd5ec3e5c78d37f7456fc98e9f94f648259dcd62
FelipeFerreiraRodrigues <felipeferreirarodrigues.2009@gmail.com>
2026-06-03 14:30:09 -0300

    adicionando arquivo vazio

---
+++ smartlibrary.py
@@ -0,0 +1,9 @@
+print ("""==================================================
+        BIBLIOTECA COMUNITÁRIA - CLI v1.0
+==================================================
+1. Ver Catálogo de Livros (Livre)
+2. Criar Nova Conta (Gerar Registro)
+3. Fazer Login
+4. Sair
+--------------------------------------------------
+Escolha uma opção: _""")
